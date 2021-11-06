bfdda661ca389d15eb959081608d7e71de35312a
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2021-11-06 23:59:17 +0530

    host change

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,8 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
-        # 'CONFIG': {
-        #     'hosts': [('127.0.0.1', 6379)],
-        # }
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 8000)],
+        }
     }
 }
